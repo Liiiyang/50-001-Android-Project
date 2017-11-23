f3dd3ca6881e2a64eb5e43711555507c23778b3d
Liiiyang <tanliyang3000@hotmail.com>
2017-11-23 13:45:45 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/liyang/androidtouristapp/AttractionSelection.java
@@ -0,0 +1,18 @@
+package com.example.liyang.androidtouristapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Li Yang on 21/11/2017.
+ */
+
+public class AttractionSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attraction_selection);
+
+    }
+}
---
+++ app/src/main/java/com/example/liyang/androidtouristapp/RecyclerAttraction.java
@@ -0,0 +1,10 @@
+package com.example.liyang.androidtouristapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Li Yang on 22/11/2017.
+ */
+
+public class RecyclerAttraction extends AppCompatActivity {
+}
